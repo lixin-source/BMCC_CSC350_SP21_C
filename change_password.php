71d3adc1503a48b718b80394e7f9f2c84ace4d13
Hyunsoo Lee <hsleenet@gmail.com>
2021-04-21 18:21:28 +0000

    20210421 14:19

--- change_password.php
+++ change_password.php
@@ -58,7 +58,6 @@ if ($isUser){
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Bootstrap contact form with PHP example by BootstrapBay.com.">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <style>
 h1{
