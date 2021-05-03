6e68b09a92a2ca350883b55bdb937ac01e335ac2
Hyunsoo Lee <56422173+kylehslee@users.noreply.github.com>
2021-05-03 13:14:02 -0400

    Update current.php

--- current.php
+++ current.php
@@ -15,10 +15,7 @@
 <?php
 function SetTable()
 {
-$username = "newuser"; 
-$password = "password"; 
-$database = "csc350"; 
-$mysqli = new mysqli("localhost", $username, $password, $database); 
+	include_once('db.php');
 
 
 $arr;
