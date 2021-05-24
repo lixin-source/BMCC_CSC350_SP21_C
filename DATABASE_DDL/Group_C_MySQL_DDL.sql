9aa676ab54fe08ed73acbb1b57dc9760b5e6ad32
Hyunsoo-Ubuntu <hsleenet@gmail.com>
2021-05-24 13:51:21 -0400

    20210524 13:51

--- DATABASE_DDL/Group_C_MySQL_DDL.sql
+++ DATABASE_DDL/Group_C_MySQL_DDL.sql
@@ -1,77 +1,24 @@
--- MySQL dump 10.13  Distrib 5.7.34, for Linux (x86_64)
---
--- Host: localhost    Database: csc350
--- ------------------------------------------------------
--- Server version	5.7.34-0ubuntu0.18.04.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `data`
---
-
-DROP TABLE IF EXISTS `data`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `data` (
-  `date` date NOT NULL,
-  `time` int(11) NOT NULL,
-  `apt` varchar(3) NOT NULL,
-  `name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `data`
---
-
-LOCK TABLES `data` WRITE;
-/*!40000 ALTER TABLE `data` DISABLE KEYS */;
-INSERT INTO `data` VALUES ('2021-04-19',4,'21D','af'),('2021-04-19',5,'12D','cvb'),('2021-04-28',27,'1A','jeff'),('2021-05-12',0,'10A','xinyi'),('2021-05-12',1,'1A','jeff'),('2021-05-21',8,'1A','jeff');
-/*!40000 ALTER TABLE `data` ENABLE KEYS */;
+SHOW DATABASES;
 UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
+DROP DATABASE csc350;
+CREATE DATABASE csc350;
+use csc350;
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `name` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `apt` varchar(4) NOT NULL,
   PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `users`
---
+DROP TABLE IF EXISTS `data`;
+CREATE TABLE `data` (
+  `date` DATE NOT NULL,
+  `time` int NOT NULL,
+  `apt` varchar(3) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  FOREIGN KEY (`name`) REFERENCES `users` (`name`)
+);
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('hyunsoo lee','0000','21D'),('jeff','abcd','1A'),('pablo','abc','11B'),('xinyi','abc','10A');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-05-21 21:34:28
+INSERT INTO `users` values ('jeff', 'abcd', '1A'), ('hyunsoo', 'abcd', '32D');
